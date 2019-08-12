7cd2532fb9332dbfca03d5dc4dc0115f3e0e57ca
Alexander Sirota <lambdatamer@gmail.com>
2019-08-12 15:32:38 +0300

    Simplify buildscript

--- components/build.gradle.kts
+++ components/build.gradle.kts
@@ -1,15 +1,71 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    id("kotlin-android-extensions")
+}
+
+android {
+
+    compileSdkVersion(28)
+
+    defaultConfig {
+        applicationId = "com.justai.aimybox.assistant"
+
+        minSdkVersion(21)
+        targetSdkVersion(28)
+
+        versionName = "0.4.4"
+        versionCode = 1
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    buildTypes {
+        getByName("debug") {
+            isMinifyEnabled = false
+        }
+        getByName("release") {
+            //TODO configure pro guard
+        }
+    }
+
+    lintOptions {
+        isCheckAllWarnings = true
+        isWarningsAsErrors = false
+        isAbortOnError = true
+    }
+}
+
+repositories {
+    mavenLocal()
+    google()
+    jcenter()
+    mavenCentral()
+    maven("https://kotlin.bintray.com/kotlinx")
+    maven("https://dl.bintray.com/aimybox/aimybox-android-sdk/")
+}
+
 dependencies {
-    implementation(Libraries.Android.appCompat)
-    implementation(Libraries.Android.constraintLayout)
-    implementation(Libraries.Android.ktx)
-    implementation(Libraries.Android.material)
-    implementation(Libraries.Android.lifecycle)
 
-    implementation(Libraries.Kotlin.stdLib)
-    batchImplementation(Libraries.Kotlin.coroutines)
+    implementation("androidx.appcompat:appcompat:1.0.2")
+    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+    implementation("androidx.core:core-ktx:1.2.0-alpha03")
+    implementation("com.google.android.material:material:1.1.0-alpha09")
+    implementation("androidx.lifecycle:lifecycle-extensions:2.0.0")
 
-    implementation("com.justai.aimybox:core:${Versions.aimybox}")
+    implementation(kotlin("stdlib"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.0-M1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.0-M1")
 
-    implementation(Libraries.Android.glide)
-    annotationProcessor(Libraries.Android.glideCompiler)
+    implementation("com.github.bumptech.glide:glide:4.9.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.9.0")
+
+    val aimyboxVersion: String by rootProject.extra
+
+    implementation("com.justai.aimybox:core:$aimyboxVersion")
+    implementation("com.justai.aimybox:google-platform-speechkit:$aimyboxVersion")
+
 }
