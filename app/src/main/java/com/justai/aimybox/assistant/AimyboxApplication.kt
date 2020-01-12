965db7352ebed4abbd666fffb237c8bd19e8170b
morfeusys <che@just-ai.com>
2020-01-12 18:43:05 +0300

    kaldi speechkit

--- app/src/main/java/com/justai/aimybox/assistant/AimyboxApplication.kt
+++ app/src/main/java/com/justai/aimybox/assistant/AimyboxApplication.kt
@@ -3,11 +3,13 @@ package com.justai.aimybox.assistant
 import android.app.Application
 import android.content.Context
 import com.justai.aimybox.Aimybox
-import com.justai.aimybox.api.aimybox.AimyboxDialogApi
 import com.justai.aimybox.components.AimyboxProvider
 import com.justai.aimybox.core.Config
+import com.justai.aimybox.dialogapi.command.CommandDialogApi
 import com.justai.aimybox.speechkit.google.platform.GooglePlatformSpeechToText
 import com.justai.aimybox.speechkit.google.platform.GooglePlatformTextToSpeech
+import com.justai.aimybox.speechkit.kaldi.KaldiAssets
+import com.justai.aimybox.speechkit.kaldi.KaldiSpeechToText
 import java.util.*
 
 class AimyboxApplication : Application(), AimyboxProvider {
@@ -21,10 +23,13 @@ class AimyboxApplication : Application(), AimyboxProvider {
     private fun createAimybox(context: Context): Aimybox {
         val unitId = UUID.randomUUID().toString()
 
-        val textToSpeech = GooglePlatformTextToSpeech(context, Locale.ENGLISH)
-        val speechToText = GooglePlatformSpeechToText(context, Locale.ENGLISH)
+        val textToSpeech = GooglePlatformTextToSpeech(context, Locale.getDefault())
+        val speechToText = KaldiSpeechToText(KaldiAssets.fromApkAssets(this, "model"))
 
-        val dialogApi = AimyboxDialogApi(AIMYBOX_API_KEY, unitId)
+        val dialogApi = CommandDialogApi(
+            this,
+            listOf(R.xml.commands),
+            linkedSetOf(EchoSkill()))
 
         return Aimybox(Config.create(speechToText, textToSpeech, dialogApi))
     }
