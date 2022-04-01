a8d6641674430b2fbc1cfebe68dd5a3e351cc3ef
AlexeyS32 <sakovikh@gmail.com>
2022-04-01 13:30:35 +0300

    AGP updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,13 +7,13 @@ plugins {
 
 android {
 
-    compileSdkVersion(31)
+    compileSdk = 31
 
     defaultConfig {
         applicationId = "com.justai.aimybox.assistant"
 
-        minSdkVersion(21)
-        targetSdkVersion(31)
+        minSdk = 21
+        targetSdk = 31
 
         versionName = aimyboxVersion
         versionCode = 1
@@ -32,36 +32,27 @@ android {
             //TODO configure pro guard
         }
     }
-
-    lintOptions {
-        isCheckAllWarnings = true
-        isWarningsAsErrors = false
-        isAbortOnError = true
+    lint {
+        abortOnError = true
+        checkAllWarnings = true
+        warningsAsErrors = false
     }
-}
-
-repositories {
-    mavenLocal()
-    google()
-    jcenter()
-    mavenCentral()
-    maven("https://kotlin.bintray.com/kotlinx")
 }
 
 dependencies {
 
     debugImplementation("com.squareup.leakcanary:leakcanary-android:2.0-beta-3")
 
-    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("androidx.appcompat:appcompat:1.4.1")
     implementation("androidx.recyclerview:recyclerview:1.2.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
     implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
 
-    implementation(kotlin("stdlib"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.0")
 
     implementation("com.just-ai.aimybox:components:$aimyboxVersion")
     implementation("com.just-ai.aimybox:core:$aimyboxVersion")
     implementation("com.just-ai.aimybox:google-platform-speechkit:$aimyboxVersion")
+
 }
